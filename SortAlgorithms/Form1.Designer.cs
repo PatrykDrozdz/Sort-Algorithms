5cd3ad26e47d0458f2a40993773fd2d574d1041c
PatrykDrozdz <pdrozdz@onet.eu>
2016-05-18 00:17:06 +0200

    first, doesn't work

---
+++ SortAlgorithms/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace SortAlgorithms
+{
+    partial class Sorts
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btime = new System.Windows.Forms.Label();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.buble = new System.Windows.Forms.RadioButton();
+            this.sort = new System.Windows.Forms.Button();
+            this.gener = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btime);
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.buble);
+            this.groupBox1.Location = new System.Drawing.Point(474, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(294, 276);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sorty";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(188, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "BTime";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(17, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "BTime";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(188, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "BTime";
+            // 
+            // btime
+            // 
+            this.btime.AutoSize = true;
+            this.btime.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btime.Location = new System.Drawing.Point(17, 62);
+            this.btime.Name = "btime";
+            this.btime.Size = new System.Drawing.Size(65, 24);
+            this.btime.TabIndex = 3;
+            this.btime.Text = "BTime";
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(192, 102);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(85, 17);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "radioButton4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(21, 102);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(85, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(192, 28);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // buble
+            // 
+            this.buble.AutoSize = true;
+            this.buble.Location = new System.Drawing.Point(21, 28);
+            this.buble.Name = "buble";
+            this.buble.Size = new System.Drawing.Size(74, 17);
+            this.buble.TabIndex = 0;
+            this.buble.TabStop = true;
+            this.buble.Text = "Buble Sort";
+            this.buble.UseVisualStyleBackColor = true;
+            // 
+            // sort
+            // 
+            this.sort.Location = new System.Drawing.Point(474, 315);
+            this.sort.Name = "sort";
+            this.sort.Size = new System.Drawing.Size(106, 64);
+            this.sort.TabIndex = 1;
+            this.sort.Text = "Sortuj";
+            this.sort.UseVisualStyleBackColor = true;
+            this.sort.Click += new System.EventHandler(this.sort_Click);
+            // 
+            // gener
+            // 
+            this.gener.Location = new System.Drawing.Point(630, 315);
+            this.gener.Name = "gener";
+            this.gener.Size = new System.Drawing.Size(121, 64);
+            this.gener.TabIndex = 2;
+            this.gener.Text = "Generuj";
+            this.gener.UseVisualStyleBackColor = true;
+            this.gener.Click += new System.EventHandler(this.gener_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 10;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Sorts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 578);
+            this.Controls.Add(this.gener);
+            this.Controls.Add(this.sort);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(798, 616);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(798, 616);
+            this.Name = "Sorts";
+            this.Text = "Sort Algorithms";
+            this.Load += new System.EventHandler(this.Sorts_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton buble;
+        private System.Windows.Forms.Button sort;
+        private System.Windows.Forms.Button gener;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label btime;
+    }
+}
+
---
+++ SortAlgorithms/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SortAlgorithms
+{
+    public partial class Sorts : Form
+    {
+        private int N;
+        private int ticks;
+        private int[] tab;
+        private int[] addTab;
+
+        private Pen pen = new Pen(System.Drawing.Color.Red);
+
+        private Pen pen2 = new Pen(System.Drawing.Color.Blue);
+        private System.Random x = new Random();
+
+        public Sorts()
+        {
+            InitializeComponent();
+            this.N = this.Height;
+            this.ticks = 0;
+        }
+
+        private void gener_Click(object sender, EventArgs e)
+        {
+            System.Drawing.Graphics graph = this.CreateGraphics();
+
+            for(int i=0; i< N; i++)
+            {
+                graph.DrawLine(pen, 0, addTab[i], tab[i], addTab[i]);
+            }
+        }
+
+        private void Sorts_Load(object sender, EventArgs e)
+        {
+            
+            tab = new int[N];
+            addTab = new int[N];
+
+            for(int i=0; i< N; i++)
+            {
+                addTab[i] = i;
+                tab[i] = x.Next(1, 400);
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            System.Drawing.Graphics graph = this.CreateGraphics();
+            graph.DrawLine(pen2, 0, addTab[ticks], tab[ticks], addTab[ticks]);
+            ticks++;
+
+            this.btime.Text = ticks.ToString();
+
+            if (this.ticks == this.N)
+            {
+                timer.Stop();
+            }
+
+        }
+
+        private void sort_Click(object sender, EventArgs e)
+        {
+            if (this.buble.Checked){
+
+                int temp1 = 0;
+                int temp2 = 0;
+
+                for(int i=0; i< N; i++)
+                {
+                    for(int j=0; j<N; j++)
+                    {
+                        if (tab[j] < tab[i])
+                        {
+                            temp1 = tab[i];
+                            tab[i] = tab[j];
+                            tab[j] = temp1;
+
+                            temp2 = addTab[i];
+                            addTab[i] = addTab[j];
+                            addTab[j] = temp2;
+
+                        }
+                    }
+                }
+                timer.Enabled = true;
+                timer.Start();
+
+            }
+
+        }
+    }
+}
